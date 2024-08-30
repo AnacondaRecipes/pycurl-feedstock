05199bc6224062d50a814f7727795ad4ac52ee7e
Brian Wing <bwing@anaconda.com>
2024-08-30 17:01:58 -0400

    Correct runtime deps and build environment on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ set "PYCURL_CURL_DIR=%LIBRARY_PREFIX%"
 set "PYCURL_USE_LIBCURL_DLL=1"
 set "PYCURL_SSL_LIBRARY=schannel"
 
-%PYTHON% -m pip install . --no-deps -vv
+%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
 if errorlevel 1 exit 1
 
 rd /s /q %PREFIX%\Doc
