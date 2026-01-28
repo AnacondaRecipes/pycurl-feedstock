fddc69677e7382155ecb3611c28f82bf070cb04e
Serhii Kupriienko <skupriienko@anaconda.com>
2026-01-28 11:23:47 +0200

    Remove the patch

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -7,7 +7,8 @@ pip check
 make CC=$CC -C tests/fake-curl/libcurl
 
 if [[ "$target_platform" == "osx-arm64" ]]; then
-  ulimit -n 4096 2>/dev/null || true
+    ulimit -n 4096 2>/dev/null || true
 fi
 
-pytest -v tests
+# pause/unpause test can randomly fail on Unix, see https://github.com/pycurl/pycurl/pull/921See 
+pytest -v tests -k "not test_easy_pause_unpause"
