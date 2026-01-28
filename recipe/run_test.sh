4d7b2fb6e9b8b58136089b5bdd5179d621fb56f8
Serhii Kupriienko <skupriienko@anaconda.com>
2026-01-28 10:48:36 +0200

    Rebuild for py314

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -5,4 +5,9 @@ set -euxo pipefail
 pip check
 
 make CC=$CC -C tests/fake-curl/libcurl
+
+if [[ "$target_platform" == "osx-arm64" ]]; then
+  ulimit -n 4096 2>/dev/null || true
+fi
+
 pytest -v tests
