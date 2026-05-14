f158511f930084a212d8e3117e7240f7e81e2e30
Serhii Kupriienko <skupriienko@anaconda.com>
2026-05-14 12:35:37 +0300

    Skip tests because of multi-socket / socketp ordering on Unix CI causes issues

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -10,5 +10,9 @@ if [[ "$target_platform" == "osx-arm64" ]]; then
     ulimit -n 4096 2>/dev/null || true
 fi
 
-# pause/unpause test can randomly fail on Unix, see https://github.com/pycurl/pycurl/pull/921See 
-pytest -v tests -k "not test_easy_pause_unpause"
+# Skipped tests on Unix (see pycurl upstream):
+# - test_easy_pause_unpause: flaky pause/unpause, https://github.com/pycurl/pycurl/pull/921
+# - test_multi_unassign_inside_socket_callback, test_clear_via_assign_none_inside_callback_resets_socketp:
+#   assume an extra M_SOCKETFUNCTION with cleared socketp after in-callback unassign/assign(None);
+#   libcurl + fdset/select ordering on Linux CI often completes the transfer without that callback.
+pytest -v tests -k 'not test_easy_pause_unpause and not test_multi_unassign_inside_socket_callback and not test_clear_via_assign_none_inside_callback_resets_socketp'
